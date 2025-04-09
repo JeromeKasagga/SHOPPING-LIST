2b098391359fc9c249b6cca2dd53f8048338283b
JeromeKasagga <jerometehcguy@gmail.com>
2025-04-09 14:36:13 +0300

    Fuluu functional JS 🥰

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-const inputText = document.querySelector("input-box");
+const inputText = document.querySelector("#input-box");
 const addButton = document.querySelector("button");
+const listDisplay = document.querySelector("ul");
 
 addButton.addEventListener("click", function() {
-    const shoppingItem = inputText.value
-})
+    const shoppingItem = document.createElement("li");
+    shoppingItem.textContent = inputText.value;
+
+    listDisplay.appendChild(shoppingItem);
+});
+
