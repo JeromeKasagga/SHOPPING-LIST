c5b80eab81ac534c38b6a0fab88a33cfd52d8a27
JeromeKasagga <jerometehcguy@gmail.com>
2025-04-09 14:14:14 +0300

    Basic JS

---
+++ index.js
@@ -0,0 +1,6 @@
+const inputText = document.querySelector("input-box");
+const addButton = document.querySelector("button");
+
+addButton.addEventListener("click", function() {
+    const shoppingItem = inputText.value
+})
